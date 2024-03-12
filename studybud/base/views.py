8324a8549378ecb1a3d536ea7ca790e4488fa2da
Amir Firoozi <amir.firoozi@gmail.com>
2024-03-12 16:45:08 -0400

    add some

--- studybud/base/views.py
+++ studybud/base/views.py
@@ -1,11 +1,27 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Room
+
+
+
+rooms = [
+    {'id':1, 'name': 'learn python'},
+    {'id':2, 'name': 'Java Script'},
+    {'id':3, 'name': 'Front-end developers'},
+]
 
 # Create your views here.
 def home(request):
-    return HttpResponse("home page") 
+
+    context = {'rooms': rooms}
+    return render (request, 'base/home.html',context)
 
 
 # Create your views here.
-def room(request):
-    return HttpResponse("ROOM page")
+def room(request, pk):
+    room = None
+    for i in rooms:
+        if i['id'] == int(pk):
+            room = i
+    
+    context = {'room': room}
+    return render (request, 'base/room.html', context)
