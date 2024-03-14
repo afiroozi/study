2fe0cc7aa57956c66bfaba7d80063acacf9ad307
Amir Firoozi <amir.firoozi@gmail.com>
2024-03-14 16:13:09 -0400

    day 2

--- studybud/base/views.py
+++ studybud/base/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Room
+from .forms import RoomForm
 
 
 
@@ -11,17 +12,26 @@ rooms = [
 
 # Create your views here.
 def home(request):
-
+    rooms = Room.objects.all()
     context = {'rooms': rooms}
     return render (request, 'base/home.html',context)
 
 
 # Create your views here.
 def room(request, pk):
-    room = None
-    for i in rooms:
-        if i['id'] == int(pk):
-            room = i
-    
+    room = Room.objects.get(id=pk)
     context = {'room': room}
     return render (request, 'base/room.html', context)
+
+def createRoom(request):
+
+    form = RoomForm()
+    if request.method == 'POST':
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+        
+
+
+    context = {'form': form}
+    return render(request, 'base/room_form.html', context)
