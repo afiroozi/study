2b54e9a21edc29c42089c2b54e4be1152bb12c5c
Amir Firoozi <amir.firoozi@gmail.com>
2024-03-14 22:47:32 -0400

    fix the forms

--- studybud/base/views.py
+++ studybud/base/views.py
@@ -4,30 +4,22 @@ from .forms import RoomForm
 
 
 
-rooms = [
-    {'id':1, 'name': 'learn python'},
-    {'id':2, 'name': 'Java Script'},
-    {'id':3, 'name': 'Front-end developers'},
-]
-
-# Create your views here.
 def home(request):
     rooms = Room.objects.all()
     context = {'rooms': rooms}
     return render (request, 'base/home.html',context)
 
 
-# Create your views here.
 def room(request, pk):
     room = Room.objects.get(id=pk)
     context = {'room': room}
     return render (request, 'base/room.html', context)
 
 def createRoom(request):
-
-    form = RoomForm()
+    form = RoomForm(request.POST)
     if request.method == 'POST':
         if form.is_valid():
+            print(form)
             form.save()
             return redirect('home')
         
