44fc3c095b2ea6bf7b62a60cd3d905accff5fdc6
Amir Firoozi <amir.firoozi@gmail.com>
2024-03-16 23:03:04 -0400

    topics

--- studybud/base/views.py
+++ studybud/base/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render, redirect
-from .models import Room
+from .models import Room, Topic
 from .forms import RoomForm
 
 
 
 def home(request):
-    rooms = Room.objects.all()
-    context = {'rooms': rooms}
+    q = request.GET.get('q') if request.GET.get('q') != None else '' 
+    rooms = Room.objects.filter(topic__name__icontains=q)
+    topics = Topic.objects.all()
+    context = {'rooms': rooms, 'topics': topics}
     return render (request, 'base/home.html',context)
 
 
@@ -38,4 +40,19 @@ def updateRoom(request, pk):
     
     context = {'form': form}
     return render(request, 'base/room_form.html', context)
+
+def deleteRoom(request, pk):
+    room = Room.objects.get(id=pk)
+    if request.method == 'POST':
+        room.delete()
+        return redirect('home')        
+    
+    return render(request, 'base/delete.html', {'obj': room})
+
+def topicsList(request):
+    topics = Topic.objects.all()
+    context = {'topics': topics}
+
+    return render(request, 'base/home.html', context)
+
 
