9513da4362c25e4be4af00b2ca7e258f0ef5ff30
Amir Firoozi <amir.firoozi@gmail.com>
2024-03-18 16:29:35 -0400

    login

--- studybud/base/views.py
+++ studybud/base/views.py
@@ -1,14 +1,43 @@
 from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
+from django.db.models import Q
 from .models import Room, Topic
 from .forms import RoomForm
 
 
+def loginPage(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        try:
+            user = User.objects.get(username=username)
+        except:
+            messages.error(request, 'User does not exist!')    
+
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('home') 
+
+    context = {}
+    return render(request, 'base/login_register.html', context)
+
 
 def home(request):
     q = request.GET.get('q') if request.GET.get('q') != None else '' 
-    rooms = Room.objects.filter(topic__name__icontains=q)
+
+    rooms = Room.objects.filter(
+        Q(topic__name__icontains=q) | 
+        Q(name__icontains=q) |
+        Q(description__icontains=q)
+        
+    )
+    room_count = rooms.count()
     topics = Topic.objects.all()
-    context = {'rooms': rooms, 'topics': topics}
+    context = {'rooms': rooms, 'topics': topics, 'room_count':room_count}
     return render (request, 'base/home.html',context)
 
 
